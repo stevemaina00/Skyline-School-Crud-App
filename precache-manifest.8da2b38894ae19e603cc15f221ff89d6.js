21107150ecc2788176d1c2177e96ac7781d0b1c5
Steve <steve.maina@student.moringaschool.com>
2022-11-12 01:25:04 +0300

    Updates

---
+++ precache-manifest.8da2b38894ae19e603cc15f221ff89d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bd61dbe89f7420bc811",
+    "url": "/Skyline-School-Crud-App/static/css/main.6733ea3f.chunk.css"
+  },
+  {
+    "revision": "3bd61dbe89f7420bc811",
+    "url": "/Skyline-School-Crud-App/static/js/main.3bd61dbe.chunk.js"
+  },
+  {
+    "revision": "9a706a69ef0a8c3185a3",
+    "url": "/Skyline-School-Crud-App/static/js/1.9a706a69.chunk.js"
+  },
+  {
+    "revision": "7bfc3ead6180bbcf25f4",
+    "url": "/Skyline-School-Crud-App/static/js/2.7bfc3ead.chunk.js"
+  },
+  {
+    "revision": "4a7f97bec0f4fd620dc2",
+    "url": "/Skyline-School-Crud-App/static/js/runtime~main.4a7f97be.js"
+  },
+  {
+    "revision": "b6963b473e42f2ebaab610715982fca9",
+    "url": "/Skyline-School-Crud-App/index.html"
+  }
+];
